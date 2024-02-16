ea26a5235f93615fef0fd3d2ba29ccd8863f91a6
akumaru <isakulovdev@gmail.com>
2024-02-16 17:46:09 +0500

    starter

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import LemonIcon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/lemon-2.tsx";
+
+export default function Header() {
+  const menus = [
+    { name: "About", href: "/about" },
+    { name: "Projects", href: "/projects" },
+    { name: "Blog", href: "/blog" },
+  ];
+
+  return (
+    <div class="bg-cyan-100 py-6 px-8 ">
+      <div className={"mx-auto w-4/5 flex flex-col md:flex-row gap-4"}>
+        <div class="flex items-center flex-1">
+          <LemonIcon aria-hidden="true" />
+          <div class="text-2xl ml-1 font-bold">
+            Home
+          </div>
+        </div>
+        <ul class="flex items-center gap-6">
+          {menus.map((menu) => (
+            <li>
+              <a
+                href={menu.href}
+                class={"hover:text-gray-700 py-1 border-gray-500 font-bold border-b-2"}
+              >
+                {menu.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <div className={""}>
+        <Header></Header>
+      </div>
+    </>
+  );
+}
