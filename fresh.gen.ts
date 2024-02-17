54972d9ac89e8f59168044edbb430ab48388bac6
akumaru <isakulovdev@gmail.com>
2024-02-17 12:25:45 +0500

    generate route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,7 +4,11 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $about from "./routes/about.tsx";
+import * as $blog_slug_ from "./routes/blog/[slug].tsx";
+import * as $blog_index from "./routes/blog/index.tsx";
 import * as $index from "./routes/index.tsx";
+import * as $projects from "./routes/projects.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
 
@@ -12,7 +16,11 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/about.tsx": $about,
+    "./routes/blog/[slug].tsx": $blog_slug_,
+    "./routes/blog/index.tsx": $blog_index,
     "./routes/index.tsx": $index,
+    "./routes/projects.tsx": $projects,
   },
   islands: {},
   baseUrl: import.meta.url,
