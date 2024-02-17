2afa0aaf249ec20ffc95ceb530b6b7a58ee42199
akumaru <isakulovdev@gmail.com>
2024-02-17 14:39:07 +0500

    `meta` tags & info

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,8 +5,23 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fresh-website</title>
+        <meta
+          name="description"
+          content="Konichiwa, I'm akumarujon, self-taught Developer from Uzbekistan. This is my personal website. You can find more information about me here."
+        />
+        <meta
+          name="og:description"
+          content="Konichiwa, I'm akumarujon, self-taught Developer from Uzbekistan. This is my personal website. You can find more information about me here."
+        />
+        <meta name="og:site_name" content="akumarujon UwU" />
+        <meta name="og:url" content="https://akumarujon.uz" />
+        <meta name="og:image" content="https://akumarujon.uz/og.png" />
+        <meta name="twitter:image" content="https://akumarujon.uz/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <title>akumarujon</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/font.css" />
+        <link rel="canonical" href="https://akumarujon.uz" />
       </head>
       <body>
         <Component />
