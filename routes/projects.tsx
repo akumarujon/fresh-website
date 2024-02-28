ac619dd593eac4d71829bbf5125e2eb647200fe0
akumaru <isakulovdev@gmail.com>
2024-02-28 08:08:08 +0500

    remove unused `Header`

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -3,6 +3,7 @@ import Header from "@components/Header.tsx";
 export default function () {
   return (
     <>
+      <Header></Header>{" "}
       <div className={"mx-auto w-3/4"}>
         <h1>Projects</h1>
       </div>
