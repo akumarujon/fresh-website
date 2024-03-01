798ae6e795c7e32f07bb205eed04654dcdfb9f2f
akumaru <isakulovdev@gmail.com>
2024-03-01 18:03:02 +0500

    fmt

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -1,9 +1,6 @@
-import Header from "@components/Header.tsx";
-
 export default function () {
   return (
     <>
-      <Header></Header>{" "}
       <div className={"mx-auto w-3/4"}>
         <h1>Projects</h1>
       </div>
