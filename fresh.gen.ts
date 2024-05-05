c327c1b1aa2a339ecb22f656be7d416771688eaf
akumarujon <isakulovdev@gmail.com>
2024-05-05 19:15:06 +0500

    remove photos and use 1

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,7 +7,6 @@ import * as $_app from "./routes/_app.tsx";
 import * as $blog_slug_ from "./routes/blog/[slug].tsx";
 import * as $blog_index from "./routes/blog/index.tsx";
 import * as $index from "./routes/index.tsx";
-import * as $projects from "./routes/projects.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
 
@@ -18,7 +17,6 @@ const manifest = {
     "./routes/blog/[slug].tsx": $blog_slug_,
     "./routes/blog/index.tsx": $blog_index,
     "./routes/index.tsx": $index,
-    "./routes/projects.tsx": $projects,
   },
   islands: {},
   baseUrl: import.meta.url,
